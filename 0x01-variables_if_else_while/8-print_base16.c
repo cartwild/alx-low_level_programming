2b0e6a7b6ca643c1bf4e663200c5c23b41326470
cartwild <cartweird@icloud.com>
2023-02-18 06:13:11 -0800

    Hot fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 /**
 * main - Entry point
-*
-* Description: This program prints all numbers of base64 in lowercase using
+* Description: prints all numbers of base64 in lowercase
 * only the `putchar` function and only 3 times.
-*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
 char c;
-int i;
-for(i = 0; i < 10; i++) 
-{
-putchar(i + '0');
-}
-for (c = 'a'; c < 'g'; c++)
+for (c = 'z'; c >= 'a'; c--)
 {
 putchar(c);
 }
