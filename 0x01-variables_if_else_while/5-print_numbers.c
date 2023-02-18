72b9a3902856b80ec22e9ac961b2c11c3c5e57d4
cartwild <cartweird@icloud.com>
2023-02-18 03:42:43 -0800

    initial commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/**               
-* main - main block
+/**
+* main- main block
 * Description: Print all numbers of base 10, starting from 0.
-* Return: 0
+* return 0
 */
 int main(void)
 {
