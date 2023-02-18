9787bd364410df949a18571ba3b38fafa2829056
cartwild <cartweird@icloud.com>
2023-02-18 03:47:14 -0800

    Hot FIX

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main- main block
-* Description: Print all numbers of base 10, starting from 0.
-* return 0
+* main - main block
+* Description: prints all numbers of base 10, starting from 0.
+* return: 0
 */
 int main(void)
 {
